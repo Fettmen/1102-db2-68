881dd48d47d479359d7a4c4b8236e54d8f5ff673
Fettmen <alanmou0122@gmail.com>
2022-03-22 15:56:21 +0800

    initial crown2_68 files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 const crown_68_Router = require('./routes/crown_68');
+const crown2_68_Router = require('./routes/crown2_68');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/crown_68',crown_68_Router);
+app.use('/crown2_68',crown2_68_Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/crown2_68.js
+++ routes/crown2_68.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('in');
-  res.render('crown_68/index', { id: '409410668' , title: 'Hongmou Chang' });
+  res.render('crown2_68/index', { id: '409410668' , title: 'Hongmou Chang' });
 });
 
 module.exports = router;
