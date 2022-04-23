238a52aade6cdb9a337564faa261f6cfe8f36247
Fettmen <alanmou0122@gmail.com>
2022-04-24 01:24:55 +0800

    424-2

--- utils/database.js
+++ utils/database.js
@@ -16,11 +16,11 @@ if(isProduction) {
 else{
     pool = new Pool(
         { 
-            user: 'postgres',
-            host: 'localhost',
+            user: 'aljuwvrnwunbro',
+            host: 'ec2-54-83-21-198.compute-1.amazonaws.com',
             port: '5432',
-            database: 'crown_68',
-            password: '0000'
+            database: 'd8ve15c9c5oom1',
+            password: '3e03339ae608f4ee8c1d2a0867be02696e29f5918949c1a1a533788f626f73d8'
         }
     );
 } 
