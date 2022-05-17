f654013e12b342fdedd06c6df3114e2d6a1f32e0
Fettmen <alanmou0122@gmail.com>
2022-05-17 16:05:48 +0800

    w13-p1: create controllers for crown2 for MVC architecture

---
+++ controllers/crown2Controller_68.js
@@ -0,0 +1,37 @@
+const Category_68 = require('../models/Category_68');
+const Shop_68 = require('../models/Shop_68');
+
+exports.getCategories = async (req,res) => {
+    try{
+        let results = await Category_68.fetchAll();
+        console.log('results', JSON.stringify(results));
+        res.render('crown2_68/index',{
+          data: results,
+          id: '409410668',
+          title: 'Crown2_DB'
+        });
+      }catch(err){
+        console.log(err);
+      }
+};
+
+exports.getProductsByCategory = async (req,res) => {
+    console.log('category',req.params.category);
+
+  try{
+    const cid = await Category_68.fetchCatIdByName(req.params.category);
+    //console.log('cid',cid);
+    //const results = Shop_68.fetchProductsByCategory(cid);
+    let results = await Shop_68.fetchProductsByCategory(cid);
+    console.log('result',JSON.stringify(results));
+    
+    res.render('crown2_68/products_68',{
+      data: results,
+      title: req.params.category,
+      name: 'Hongmou Chang',
+      id: '409410668'
+    });
+  }catch(err){
+    console.log(err);
+  }
+}
